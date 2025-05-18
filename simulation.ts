20570f5215e76015c80e967894d7f38c00c6b895
robinfaury <robinfaury@gmail.com>
2025-05-19 01:06:48 +0200

    Make the texture of the simulation sharpe

--- simulation.ts
+++ simulation.ts
@@ -60,16 +60,16 @@ export class Simulation {
       this.gl.texImage2D(this.gl.TEXTURE_2D, level, internalFormat,
         this.texture_width, this.texture_height, border, format, type, data);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.NEAREST_MIPMAP_NEAREST, this.gl.NEAREST);
       this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture_framebuffer);
       this.gl.texImage2D(this.gl.TEXTURE_2D, level, internalFormat,
         this.texture_width, this.texture_height, border, format, type, data);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
       this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
-      this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.NEAREST_MIPMAP_NEAREST, this.gl.NEAREST);
     }
     this.framebuffer = this.gl.createFramebuffer();
     this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
